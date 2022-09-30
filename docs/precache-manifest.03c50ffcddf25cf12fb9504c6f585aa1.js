b94638f81db68e6d8162e4d9c8a67f34be0e325d
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2022-09-30 13:53:14 +0700

    Init

---
+++ docs/precache-manifest.03c50ffcddf25cf12fb9504c6f585aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d05ce13dd8be073d53113dbf2b29835",
+    "url": "https://aicafe1.github.io/vnqa-sa/index.html"
+  },
+  {
+    "revision": "75b895426727e39c52ba",
+    "url": "https://aicafe1.github.io/vnqa-sa/static/css/2.5ee7db17.chunk.css"
+  },
+  {
+    "revision": "75b895426727e39c52ba",
+    "url": "https://aicafe1.github.io/vnqa-sa/static/js/2.851701ee.chunk.js"
+  },
+  {
+    "revision": "ae64cba97c68b91bd7e7d882ee6eb197",
+    "url": "https://aicafe1.github.io/vnqa-sa/static/js/2.851701ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3faaf1f59aa9e51e1bb",
+    "url": "https://aicafe1.github.io/vnqa-sa/static/js/main.86d218b8.chunk.js"
+  },
+  {
+    "revision": "945d447ee3746f5b6379",
+    "url": "https://aicafe1.github.io/vnqa-sa/static/js/runtime-main.d5812712.js"
+  }
+]);
